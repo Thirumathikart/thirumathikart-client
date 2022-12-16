a440ec4df047cbee39c0929581f5db8b10bcc294
S Selvanayagam <97583793+S-Selvanayagam@users.noreply.github.com>
2022-12-17 00:43:45 +0530

    add myOrdersPage

--- lib/bindings/orders_binding.dart
+++ lib/bindings/orders_binding.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/bindings_interface.dart';
 import 'package:thirumathikart_app/controllers/orders_controller.dart';
 
 class OrdersBindings extends Bindings {
