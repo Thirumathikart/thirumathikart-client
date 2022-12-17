2a82e78cb634cf3aa71a5e4d71249ab009275b78
https://github.com/Valliammai-SM <112121074@nitt.edu>
2022-12-17 23:49:22 +0530

    fix: formatting errors

--- lib/bindings/orders_binding.dart
+++ lib/bindings/orders_binding.dart
@@ -1,6 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_instance/src/bindings_interface.dart';
 import 'package:thirumathikart_app/controllers/orders_controller.dart';
 
 class OrdersBindings extends Bindings {
--- lib/config/navigations.dart
+++ lib/config/navigations.dart
@@ -9,7 +9,6 @@ import 'package:thirumathikart_app/bindings/splash_binding.dart';
 import 'package:thirumathikart_app/bindings/login_binding.dart';
 import 'package:thirumathikart_app/bindings/register_binding.dart';
 import 'package:thirumathikart_app/bindings/cart_binding.dart';
-import 'package:thirumathikart_app/models/orders.dart';
 import 'package:thirumathikart_app/models/product.dart';
 import 'package:thirumathikart_app/views/auth.dart';
 import 'package:thirumathikart_app/views/home.dart';
@@ -124,7 +123,7 @@ class NavigationPages {
         GetPage(
           name: NavigationRoutes.myOrdersRoute,
           page: () => const OrdersPage(),
-          arguments: List<Orders>,
+          arguments: List<Product>,
           binding: OrdersBindings(),
         ),
       ];
