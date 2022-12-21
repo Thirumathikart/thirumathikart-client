932dd334d59c7b11be43e5f5b85871caf317920e
S Selvanayagam <snsn010212@gmail.com>
2022-12-21 13:25:10 +0530

    fix: removed other model

--- lib/bindings/qr_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/instance_manager.dart';
-import 'package:thirumathikart_app/controllers/qr_controller.dart';
-
-class QrBindings extends Bindings {
-  @override
-  void dependencies() {
-    Get.put(QrController());
-  }
-}
